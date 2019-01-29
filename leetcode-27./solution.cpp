591d810683d55a3e634652d0a9b7cbab4c0f5cbe
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-01-29 21:43:26 +0800

    Update solution.cpp

--- leetcode-27./solution.cpp
+++ leetcode-27./solution.cpp
@@ -5,8 +5,9 @@ public:
         int n=nums.size();
         while(i<n){
             if(nums[i]==val){
-                nums[i]=nums[n-1];
-                n--;
+                nums[i]=nums[n-1];//将n-1的位置的元素赋值给当前元素
+                n--;//n往前移动
+                //如果n-1的位置的元素也等于val，因为i此时没有增一，所以下次循环还是会发现，再接着讲向前移动之后的n-1位置的元素赋值给当前元素
             }else{
                 i++;
             }
