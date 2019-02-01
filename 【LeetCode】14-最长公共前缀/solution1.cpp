83ae6cc8ffd44bc6aa5fbf965a41824ed5ad724a
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-01 17:50:31 +0800

    Update solution1.cpp

--- 【LeetCode】14-最长公共前缀/solution1.cpp
+++ 【LeetCode】14-最长公共前缀/solution1.cpp
@@ -1,5 +1,6 @@
 class Solution {
 //水平扫描。 我们从前往后枚举字符串的每一列，先比较每个字符串相同列上的字符（即不同字符串相同下标的字符）然后再进行对下一列的比较。
+//执行用时: 4 ms, 在Longest Common Prefix的C++提交中击败了99.97% 的用户
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.size()==0){
