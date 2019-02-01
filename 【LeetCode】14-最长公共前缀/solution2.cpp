f8b7c11fecf2f97ee42d06a95b3c9273dfce00ef
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-01 18:29:30 +0800

    Update solution2.cpp

--- 【LeetCode】14-最长公共前缀/solution2.cpp
+++ 【LeetCode】14-最长公共前缀/solution2.cpp
@@ -1,4 +1,14 @@
 class Solution {
+    /*
+    分治
+思路
+
+这个算法的思路来自于LCP操作的结合律。 我们可以发现：LCP（s1....sn）=LCP(LCP(s1....sk),LCP(sk-1....sn)),将原问题LCP（si...sj）转换为
+LCP(Si...Smid)
+,LCP(Smid+1,Sj)两个子问题从头到尾挨个比较 lcpLeft 与 lcpRight 中的字符，直到不能再匹配为止。 计算所得的 lcpLeft 与 lcpRight 最长
+公共前缀就是原问题的解 LCP
+
+    */
 //速度比较慢....
 public:
     string longestCommonPrefix(vector<string>& strs) {
