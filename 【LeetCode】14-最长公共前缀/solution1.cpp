5dbcada86db3abb876343362cf01a6fa2ac9193b
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-01 17:47:41 +0800

    Create solution1.cpp

---
+++ 【LeetCode】14-最长公共前缀/solution1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+//水平扫描。 我们从前往后枚举字符串的每一列，先比较每个字符串相同列上的字符（即不同字符串相同下标的字符）然后再进行对下一列的比较。
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0){
+            return "";
+        }
+        for(int i=0;i<strs[0].size();i++){
+            char c=strs[0][i];//从第一个字符串的第一个字符开始判断
+            for(int j=1;j<strs.size();j++){
+                if(i==strs[j].size()||strs[j][i]!=c){//循环结束条件：判断到达了某一字符串的最长长度或者某一字符串的对应字符与第一个字符串对应字符不相等
+                    return strs[j].substr(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
