0a5883def0f3c93a71880994489ed86b70de55c9
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-01 19:45:39 +0800

    Create solution.cpp

---
+++ 【LeetCode】4-寻找两个有序数组的中位数/solution.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size();
+        int n=nums2.size();
+        if(m>n){//ensure m<=n
+            vector<int> temp=nums1;
+            nums1=nums2;
+            nums2=temp;
+            int tmp=m=m;
+            m=n;
+            n=tmp;
+        }
+        int imin=0,imax=m,halfLen=(m+n+1)/2;
+        while(imin<=imax){
+            int i=(imin+imax)/2;
+            int j=halfLen-i;
+            if(i<imax&&nums2[j-1]>nums1[i]){
+                imin=i+1;
+            }else if(i>imin&&nums1[i-1]>nums2[j]){
+                imax=i-1;
+            }else{
+                int maxLeft=0;
+                if(i==0){
+                    maxLeft=nums2[j-1];
+                }else if(j==0){
+                    maxLeft=nums1[i-1];
+                }else{
+                    maxLeft=max(nums1[i-1],nums2[j-1]);
+                }
+                if((m+n)%2==1){
+                    return maxLeft;
+                }
+                
+                int minRight=0;
+                if(i==m){
+                    minRight=nums2[j];
+                }else if(j==n){
+                    minRight=nums1[i];
+                }else{
+                    minRight=min(nums1[i],nums2[j]);
+                }
+                return (maxLeft+minRight)/2.0;
+            }
+        }
+        return 0.0;
+    }
+};
