f4b5564e24ffea1e9c7efee0ab2bb783f40046c7
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-07 12:21:58 +0800

    Create solution.cpp

---
+++ 【LeetCode】43-字符串相乘/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        string res;
+        int n1=num1.size(),n2=num2.size();
+        int k=n1+n2-2;
+        vector<int> temp(n1+n2,0);
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                temp[k-i-j]+=(num1[i]-'0')*(num2[j]-'0');
+            }
+        }
+        int carray=0;
+        for(int i=0;i<n1+n2;i++){
+            temp[i]+=carray;
+            carray=temp[i]/10;
+            temp[i]%=10;
+        }
+        int i=n1+n2-1;
+        while(temp[i]==0)i--;
+        if(i<0)return "0";
+        while(i>=0){
+            res.push_back(temp[i--]+'0');
+        }
+        return res;
+    }
+};
