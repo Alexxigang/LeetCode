19404c26a7786d7b230cf00ffb9833e2278e3ca8
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-17 16:19:46 +0800

    Update solution1.cpp

--- 【LeetCode】62-不同路径/solution1.cpp
+++ 【LeetCode】62-不同路径/solution1.cpp
@@ -1,6 +1,7 @@
 class Solution {
 /*
-思路：利用动态规划
+思路：利用动态规划，用动态规划Dynamic Programming来解，我们可以维护一个二维数组dp，其中dp[i][j]表示到
+当前位置不同的走法的个数，然后可以得到递推式为: dp[i][j] = dp[i - 1][j] + dp[i][j - 1]，这里为了节省空间，我们使用一维数组dp，一行一行的刷新也可以，
 */
 public:
     int uniquePaths(int m, int n) {
