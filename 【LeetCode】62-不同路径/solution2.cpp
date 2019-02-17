14e9fed7e1e51edf1fa269e6a055515a98e36b51
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-17 16:24:07 +0800

    Create solution2.cpp

---
+++ 【LeetCode】62-不同路径/solution2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+/*
+思路二：利用组合数，这个问题背后的数学模型，其实就是机器人总共走m+n-2步，
+其中m-1步往下走，n-1步往右走，本质上就是一个组合问题，也就是从m+n-2个不同元素中每次取出m-1个元素的组合数。根据组合的计算公式，我们可以写代码来求解即可
+*/
+public:
+    int uniquePaths(int m, int n) {
+        int small=m<n?m:n;
+        double dom=1,dedom=1;
+        for(int i=1;i<=small-1;i++){
+            dedom*=i;
+            dom*=m+n-1-i;
+        }
+        return (int)(dom/dedom);
+    }
+};
