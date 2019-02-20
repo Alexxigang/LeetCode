bcb576335567adccfd8fc33c02d4151b5fadcfa9
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-20 13:31:56 +0800

    Update solution1.cpp

--- 【LeetCode】89-格雷码/solution1.cpp
+++ 【LeetCode】89-格雷码/solution1.cpp
@@ -1,4 +1,15 @@
 class Solution {
+    /*
+    Int    Grey Code    Binary
+ 　　  000        000
+ 　　  001        001
+  　 　011        010
+  　 　010        011
+  　 　110        100
+  　 　111        101
+  　 　101        110
+  　　 100        111
+    */
 public:
     vector<int> grayCode(int n) {
         vector<int> res;
