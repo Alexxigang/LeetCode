c8dccb0c0632aee086aee6cbf7513765fb710d79
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-23 14:57:06 +0800

    Update solution.cpp

--- 【LeetCode】124-二叉树中的最大路径和/solution.cpp
+++ 【LeetCode】124-二叉树中的最大路径和/solution.cpp
@@ -10,7 +10,7 @@
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        int res=INT_MIN;
+        int res=INT_MIN;//全局路径最大值放在参数中，用结果res来表示。
         helper(root,res);
         return res;
     }
@@ -18,9 +18,11 @@ public:
 private:
     int helper(TreeNode* node,int& res){
         if(!node)return 0;
+        //和0相比，取较大的那个，就是要么不加，加就要加正数
         int left=max(helper(node->left,res),0);
         int right=max(helper(node->right,res),0);
         res=max(res,left+right+node->val);
+        //返回值的定义是以当前结点为终点的path之和
         return max(left,right)+node->val;
     }
 };
