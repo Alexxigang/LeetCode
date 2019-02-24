871b98bb25e4052e9e20e4da044dce63c73f4c7b
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-02-24 13:50:00 +0800

    Create solution.cpp

---
+++ 【LeetCode】142-环形链表2/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        //利用快慢指针找到环内的某一个节点
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) break;
+        }
+        if (!fast || !fast->next) return NULL;
+        slow = head;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return fast;
+    }
+};
