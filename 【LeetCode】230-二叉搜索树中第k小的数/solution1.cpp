e6b6dda3e5d919f8d971d129542e2035415fe083
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-03-21 21:25:08 +0800

    Create solution1.cpp

---
+++ 【LeetCode】230-二叉搜索树中第k小的数/solution1.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+/*
+非递归方式，利用cnt计数，栈来存储结点
+*/
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        return  kthSmallestCore(root,k);
+    }
+
+private:
+    int kthSmallestCore(TreeNode* head,int &k){
+        TreeNode* p=head;
+        stack<TreeNode*> s;
+        //s.push(p);
+        int cnt=0;
+        while(p||!s.empty()){
+            while(p){
+                s.push(p);
+                p=p->left;
+            }
+            p=s.top();s.pop();
+            cnt++;if(cnt==k)return p->val;
+            p=p->right;
+        }
+        return 0;
+    }
+};
