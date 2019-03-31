e1c88502c0b06168906964574730a990e037cd78
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2019-03-31 21:37:39 +0800

    Create solution.cpp

---
+++ 【LeetCode】344-翻转字符串/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        int i=0,j=n-1;
+        char temp;
+        while(i<j){
+            temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;j--;
+        }
+    }
+};
